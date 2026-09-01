d0ea709239e7c842158efa9026024583452484bd
Okmanbro2 <103367850+Okmanbro2@users.noreply.github.com>
2026-09-01 17:00:48 -0400

    Update ScoreboardComponent.js

--- src/gameClasses/components/ui/ScoreboardComponent.js
+++ src/gameClasses/components/ui/ScoreboardComponent.js
@@ -185,7 +185,7 @@ var ScoreboardComponent = TaroEntity.extend({
 				}
 			} else {
 				if (leaderboardToggleElement) {
-					leaderboardToggleElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="leaderboard-arrow">>
+					leaderboardToggleElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="leaderboard-arrow">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
 					</svg>`;
 				}
